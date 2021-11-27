7b6ee9212c7a88cbd73923aa9c32dd8681f2ac9d
kirdinli <semih.kirdinli@solviads.com>
2021-11-27 03:27:41 +0300

    document added

--- src/main/exercise1/JavaLambdaExpressionWithMultipleParameter.java
+++ src/main/exercise1/JavaLambdaExpressionWithMultipleParameter.java
@@ -1,19 +1,19 @@
 package exercise1;
 
 @FunctionalInterface
-interface WithNoParameter {
+interface WithMultipleParameter {
     String sayHello(String name, String surname);
 }
 
-public class JavaLambdaExpressionWithNoParameter {
+public class JavaLambdaExpressionWithMultipleParameter {
 
 
     public static void main(String[] args) {
-        WithNoParameter msg = (name, surname) -> {
+        WithMultipleParameter msg = (name, surname) -> {
             return "Hello " + name +" "+  surname;
         };
 
-        System.out.println(msg.sayHello("Yiğit", "Avcı"));
+        System.out.println(msg.sayHello("Harold", "Finch"));
 
     }
 
